b5c26ee631165f9bb5bd7cc7acfd426ca47cd74c
Andreas Chari <2293299C@student.gla.ac.uk>
2019-11-17 11:43:12 +0000

    created basic project structure

---
+++ src/components/Grid.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Grid extends Component {
+    render() {
+        return (
+            <div>
+                Grid
+            </div>
+        )
+    }
+}
+
+export default Grid
