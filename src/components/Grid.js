3908726d3e7b51e7e0391be2e5b85196251de901
Andreas Chari <2293299C@student.gla.ac.uk>
2019-11-17 12:48:13 +0000

    finished styling and Grid component, added Box component

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -1,10 +1,22 @@
 import React, { Component } from 'react'
-
+import Box from './Box'
 export class Grid extends Component {
     render() {
+            
+        const width = this.props.cols * 16 + 1
+        var rowsArray = []
+
+        var boxClass = ""
+        for(var i = 0; i < this.props.rows; i++)
+            for(var j = 0; j < this.props.cols; j++)
+                {
+                    let boxId = i + "_" + j
+                    boxClass = this.props.gridFull[i][j] ? "box on" : "box off"
+                    rowsArray.push( < Box boxClass = {boxClass} key = {boxId} boxId = {boxId} row = {i} col = {j} selectBox = {this.props.selectBox}/>)
+                }
         return (
-            <div>
-                Grid
+            <div className="grid" style={{ width: width}}>
+                {rowsArray}
             </div>
         )
     }
